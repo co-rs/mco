a1ad17aed08ae053670f5bd0e1387b16053e11eb
zhuxiujia@qq.com <zhuxiujia@qq.com>
2021-12-26 19:07:34 +0800

    add example

--- examples/src/channel.rs
+++ examples/src/channel.rs
@@ -1,8 +1,16 @@
+use std::time::Duration;
+use cogo::coroutine::sleep;
+use cogo::go;
 use cogo::std::channel::unbounded;
 
 fn main() {
     let (s,r) = unbounded();
-    s.send(1);
+    go!(move ||{
+         println!("will sleep 1s");
+         sleep(Duration::from_secs(1));
+         println!("send");
+         s.send(1);
+    });
     let rv=r.recv().unwrap();
-    println!("{}",rv);
+    println!("recv = {}",rv);
 }
