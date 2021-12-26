a628382dfe20f232b877d708b725bb9d1265c09a
zhuxiujia@qq.com <zhuxiujia@qq.com>
2021-12-26 18:24:49 +0800

    add errors

---
+++ src/std/errors/mod.rs
@@ -0,0 +1,68 @@
+use std::fmt::{self, Debug, Display};
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub struct Error {
+    pub inner: String,
+}
+
+impl Error {
+    pub fn error(&self) -> String {
+        self.inner.clone()
+    }
+}
+
+///new error
+pub fn new(text: String) -> Error {
+    Error {
+        inner: text
+    }
+}
+
+pub trait FromError<T>: Sized {
+    fn from_err(_: T) -> Error;
+}
+
+impl ToString for Error {
+    fn to_string(&self) -> String {
+        self.inner.clone()
+    }
+}
+
+impl From<std::io::Error> for Error {
+    #[inline]
+    fn from(err: std::io::Error) -> Self {
+        new(err.to_string())
+    }
+}
+
+impl From<&str> for Error {
+    fn from(arg: &str) -> Self {
+        return new(arg.to_string());
+    }
+}
+
+impl From<std::string::String> for Error {
+    fn from(arg: String) -> Self {
+        return new(arg);
+    }
+}
+
+impl From<&dyn std::error::Error> for Error {
+    fn from(arg: &dyn std::error::Error) -> Self {
+        return new(arg.to_string());
+    }
+}
+
+impl From<Box<dyn std::error::Error>> for Error {
+    fn from(arg: Box<dyn std::error::Error>) -> Self {
+        return new(arg.to_string());
+    }
+}
+
+impl From<&Box<dyn std::error::Error>> for Error {
+    fn from(arg: &Box<dyn std::error::Error>) -> Self {
+        return new(arg.to_string());
+    }
+}
--- src/std/mod.rs
+++ src/std/mod.rs
@@ -2,3 +2,4 @@ pub mod channel;
 pub mod queue;
 pub mod sync;
 pub mod http;
+pub mod errors;
