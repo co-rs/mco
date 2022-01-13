2cfb32d510496fb1dfee61edd842252ca89ed34e
zhuxiujie <zhuxiujia@qq.com>
2022-01-13 14:54:45 +0800

    add read_all

--- src/std/io/io.rs
+++ src/std/io/io.rs
@@ -1,3 +1,4 @@
+use std::io::{Read, Write};
 use crate::std::errors::Result;
 
 // Closer is the interface that wraps the basic Close method.
@@ -6,4 +7,28 @@ use crate::std::errors::Result;
 // Specific implementations may document their own behavior.
 pub trait Closer {
     fn close(&mut self) -> Result<()>;
+}
+
+pub trait ReadCloser: Read + Closer {}
+
+pub trait WriteCloser: Write + Closer {}
+
+struct NopCloser<R> where R: Read {
+    pub reader: R,
+}
+
+impl<R: std::io::Read> Closer for NopCloser<R> {
+    fn close(&mut self) -> crate::std::errors::Result<()> {
+        Ok(())
+    }
+}
+
+/// ReadAll reads from r until an error or EOF and returns the data it read.
+/// A successful call returns err == nil, not err == EOF. Because ReadAll is
+/// defined to read from src until EOF, it does not treat an EOF from Read
+/// as an error to be reported.
+pub fn read_all<R: Read>(mut r: R) -> Result<Vec<u8>> {
+    let mut b = Vec::with_capacity(512);
+    r.read_to_end(&mut b)?;
+    return Ok(b);
 }
