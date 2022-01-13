e88328fc74aa89340d3815febb61e6471fc18745
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-01-14 00:48:05 +0800

    add time

--- examples/src/time.rs
+++ examples/src/time.rs
@@ -9,7 +9,11 @@ fn main() {
     let from_js = serde_json::from_str::<Time>(&js).unwrap();
     assert_eq!(from_js, t);
 
-    t.add(1*24*Duration::from_secs(3600));// add one day
-    println!("add one day:{}",t);
+    t.add(1 * 24 * Duration::from_secs(3600));// add one day
+    println!("add one day:{}", t);
     assert_ne!(from_js, t);
+
+    assert_eq!(true, t.before(&Time::now())); //befor
+
+    assert_eq!(true, Time::now().after(&t)); //after
 }
