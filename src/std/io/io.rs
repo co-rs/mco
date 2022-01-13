e93cd70152acd6ab6599f337b3a07b517b8f3127
zhuxiujie <zhuxiujia@qq.com>
2022-01-13 14:05:01 +0800

    add closer

--- src/std/io/io.rs
+++ src/std/io/io.rs
@@ -1,9 +1,9 @@
-use crate::std::errors::Error;
+use crate::std::errors::Result;
 
 // Closer is the interface that wraps the basic Close method.
 //
 // The behavior of Close after the first call is undefined.
 // Specific implementations may document their own behavior.
 pub trait Closer {
-    fn close(&mut self) -> Error;
+    fn close(&mut self) -> Result<()>;
 }
