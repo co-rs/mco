f5a05ee118c13e201c770d611f23c1de06dd4576
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-02-02 03:40:04 +0800

    edit doc

--- benches/chan.rs
+++ benches/chan.rs
@@ -2,16 +2,70 @@
 
 #[cfg(all(nightly, test))]
 mod bench {
-    #![feature(test)]
+    #![cfg(nightly)]
     extern crate test;
 
+    use std::sync::Arc;
     use self::test::Bencher;
 
-    use super::*;
+
     use std::sync::mpsc::channel;
-    use std::sync::Arc;
     use std::thread;
-    use cogo::std::queue::spsc::Queue;
+    use cogo::std::queue::mpmc_bounded::Queue;
+
+    #[bench]
+    fn bounded_mpmc(b: &mut Bencher) {
+        b.iter(|| {
+            let total_work = 1000_000;
+            let nthreads = 1;
+            let nmsgs = total_work / nthreads;
+            let q = Queue::with_capacity(nthreads * nmsgs);
+            assert_eq!(None, q.pop());
+            let (tx, rx) = channel();
+
+            for _ in 0..nthreads {
+                let q = q.clone();
+                let tx = tx.clone();
+                thread::spawn(move || {
+                    let q = q;
+                    for i in 0..nmsgs {
+                        assert!(q.push(i).is_ok());
+                    }
+                    tx.send(()).unwrap();
+                });
+            }
+
+            let mut completion_rxs = vec![];
+            for _ in 0..nthreads {
+                let (tx, rx) = channel();
+                completion_rxs.push(rx);
+                let q = q.clone();
+                thread::spawn(move || {
+                    let q = q;
+                    let mut i = 0;
+                    loop {
+                        match q.pop() {
+                            None => {}
+                            Some(_) => {
+                                i += 1;
+                                if i == nmsgs {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    tx.send(i).unwrap();
+                });
+            }
+
+            for rx in completion_rxs.iter_mut() {
+                assert_eq!(nmsgs, rx.recv().unwrap());
+            }
+            for _ in 0..nthreads {
+                rx.recv().unwrap();
+            }
+        });
+    }
 
     #[cfg(test)]
     mod test_queue {
@@ -135,6 +189,36 @@ mod bench {
 
             for i in 0..total_work {
                 assert_eq!(i, rx.recv().unwrap());
+            }
+        });
+    }
+
+
+    // improve performance  from 39,294 ns/iter to 12,207 ns/iter (my computer)
+//test bench_channel  ... bench:      12,207 ns/iter (+/- 118)
+    #[bench]
+    fn bench_channel(b: &mut Bencher) {
+        b.iter(|| {
+            let (s, r) = channel();
+            for _ in 0..1000 {
+                s.send(1);
+            }
+            for _ in 0..1000 {
+                let r = r.recv().unwrap();
+            }
+        });
+    }
+
+    //test bench_channel2 ... bench:      39,294 ns/iter (+/- 639)
+    #[bench]
+    fn bench_channel2(b: &mut Bencher) {
+        b.iter(|| {
+            let (s, r) = channel();
+            for _ in 0..1000 {
+                s.send(1);
+            }
+            for _ in 0..1000 {
+                let r = r.recv().unwrap();
             }
         });
     }
