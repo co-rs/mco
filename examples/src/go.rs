8ebe46c5a4f70b38915c11d5a5dcc983c8bb50ab
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-02-02 20:00:34 +0800

    remove unsafe methods

--- examples/src/go.rs
+++ examples/src/go.rs
@@ -1,4 +1,4 @@
-use cogo::coroutine::{Builder, spawn};
+use cogo::coroutine::{Builder, spawn, Spawn};
 use cogo::go;
 
 fn main() {
@@ -8,17 +8,21 @@ fn main() {
     go!(2*4096,||{
        println!("go with stack size: {}",cogo::coroutine::current().stack_size());
     });
+    (2 * 4096).spawn(|| {
+        println!("go with stack size: {}", cogo::coroutine::current().stack_size());
+    });
     go!("go",||{
        println!("go with name: {}",cogo::coroutine::current().name().unwrap());
+    });
+    "go".spawn(|| {
+        println!("go with name: {}", cogo::coroutine::current().name().unwrap());
     });
     go!(Builder::new(),||{
        println!("go with Builder");
     });
-
     Builder::new().spawn(|| {
         println!("go with Builder::spawn");
     });
-
     spawn(|| {
         println!("go with method spawn");
     });
