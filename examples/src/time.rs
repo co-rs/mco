a5c1708eb50243e284ab051ac07f20414f1167aa
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-02-04 16:36:57 +0800

    edit example

--- examples/src/time.rs
+++ examples/src/time.rs
@@ -3,53 +3,53 @@ use cogo::std::time::time::Time;
 use cogo::std::time::time;
 
 fn main() {
-    let mut t = Time::now();
-    println!("{}", t);
-    println!("{:?}", t);
-    println!("{}", t.unix());
-    println!("{}", t.unix_nano());
+    let mut now = Time::now();
+    println!("{}", now);
+    println!("{:?}", now);
+    println!("{}", now.unix());
+    println!("{}", now.unix_nano());
 
     //json
-    let js = serde_json::json!(&t).to_string();
+    let js = serde_json::json!(&now).to_string();
     println!("{}", js);
     let from_js = serde_json::from_str::<Time>(&js).unwrap();
-    assert_eq!(from_js, t);
+    assert_eq!(from_js, now);
 
     //add 1 day
-    t.add(1 * 24 * Duration::from_secs(3600));
-    println!("added 1 day:{}", t);
-    assert_ne!(from_js, t);
+    now.add(1 * 24 * Duration::from_secs(3600));
+    println!("added 1 day:{}", now);
+    assert_ne!(from_js, now);
 
     //is before?
-    assert_eq!(true, t.before(&Time::now()));
+    assert_eq!(true, now.before(&Time::now()));
 
     //is after?
-    assert_eq!(true, Time::now().after(&t));
+    assert_eq!(true, Time::now().after(&now));
 
     //parse from str
-    let parsed = Time::parse(time::RFC3339Nano, &t.to_string()).unwrap();
-    assert_eq!(t, parsed);
+    let parsed = Time::parse(time::RFC3339Nano, &now.to_string()).unwrap();
+    assert_eq!(now, parsed);
 
     //format time to str
-    let formatted = t.format(time::RFC3339);
+    let formatted = now.format(time::RFC3339);
     println!("formatted: {}", formatted);
 
-    let formatted = t.format(time::RFC3339Nano);
+    let formatted = now.format(time::RFC3339Nano);
     println!("formatted: {}", formatted);
 
-    let formatted = t.format("[year]-[month] [ordinal] [weekday] [week_number]-[day] [hour]:[minute] [period]:[second].[subsecond] [offset_hour sign:mandatory]:[offset_minute]:[offset_second]");
+    let formatted = now.format("[year]-[month] [ordinal] [weekday] [week_number]-[day] [hour]:[minute] [period]:[second].[subsecond] [offset_hour sign:mandatory]:[offset_minute]:[offset_second]");
     println!("formatted: {}", formatted);
 
-    let formatted = t.format(time::RFC1123);
+    let formatted = now.format(time::RFC1123);
     println!("formatted: {}", formatted);
 
-    let formatted = t.utc();
+    let formatted = now.utc();
     println!("to utc: {}", formatted);
-    assert_eq!(t, formatted.local());
+    assert_eq!(now, formatted.local());
 
-    let formatted = t.local();
+    let formatted = now.local();
     println!("to local: {}", formatted);
-    assert_eq!(t, formatted);
+    assert_eq!(now, formatted);
 
     println!("default(): {}", Time::default());
     assert_eq!(true, Time::default().is_zero());
