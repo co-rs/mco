fe27ece6902a3b51fa2bf9c6fc275905bd914b89
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-02-13 06:30:09 +0800

    remove dead code

--- src/std/queue/mod.rs
+++ src/std/queue/mod.rs
@@ -1,10 +1,6 @@
 #![cfg_attr(all(nightly, test), feature(test))]
 
-mod block_node;
-pub mod mpmc_bounded;
 pub mod mpsc_list;
 pub mod mpsc_list_v1;
 pub mod seg_queue;
 pub mod array_queue;
-
-pub use crate::std::queue::block_node::BLOCK_SIZE;
