5964a2eaf88b14ca9806bb105f0301080a0ef943
zxj <zhuxiujia@qq.com>
2022-02-18 02:46:29 +0800

    edit example

--- examples/src/defer.rs
+++ examples/src/defer.rs
@@ -1,20 +1,29 @@
+use std::cell::RefCell;
 use mco::coroutine::sleep;
 use mco::{defer, co};
 
 fn main() {
-    defer!{
+    defer! {
         println!("guard: 1");
-    };
+    }
+    ;
     defer!(||{
         println!("guard: 2");
     });
     defer! {
         println!("guard: 3");
     }
-    defer! {
-        co!(||{
-            println!("-----------------go spawn success-------------------");
-        });
+    let complete = RefCell::new(false);
+    let c = complete.clone();
+    defer!(move|| {
+        if c.borrow().eq(&false){
+            *c.borrow_mut() = true;
+            println!("tx complete");
+        }
+    });
+    //if panic,defer will make sure complete
+    if true {
+        panic!("None Exception!");
     }
-    panic!("None Exception!");
+    *complete.borrow_mut() = true;
 }
