6cb3786fc34eaf21956fcc5f77446aaac367614c
zhuxiujia@qq.com <zhuxiujia@qq.com>
2023-12-03 17:17:54 +0800

    clear warn

--- examples/src/co.rs
+++ examples/src/co.rs
@@ -6,70 +6,70 @@ fn main() {
     co!(|| {
         println!("coroutine");
     });
-    co!(2 * 4096, || {
-        println!(
-            "coroutine with stack size: {}",
-            mco::coroutine::current().stack_size()
-        );
-    });
-    (2 * 4096).spawn(|| {
-        println!(
-            "coroutine with stack size: {}",
-            mco::coroutine::current().stack_size()
-        );
-    });
-    co!("coroutine", || {
-        println!(
-            "coroutine with name: {}",
-            mco::coroutine::current().name().unwrap_or_default()
-        );
-    });
-    "coroutine".spawn(|| {
-        println!(
-            "coroutine with name: {}",
-            mco::coroutine::current().name().unwrap_or_default()
-        );
-    });
-    co!(Builder::new(), || {
-        println!("coroutine with Builder");
-    });
-    Builder::new().spawn(|| {
-        println!("coroutine with Builder::spawn");
-    });
-    spawn(|| {
-        println!("coroutine with method spawn");
-    });
-    //yield example
-    co!(move || {
-        println!("hi, I'm parent");
-        let v = (0..100)
-            .map(|i| {
-                co!(move || {
-                    println!("hi, I'm child{:?}", i);
-                    yield_now();
-                    println!("bye from child{:?}", i);
-                })
-            })
-            .collect::<Vec<_>>();
-        yield_now();
-        // wait child finish
-        for i in v {
-            i.join().unwrap();
-        }
-        println!("bye from parent");
-    })
-    .join()
-    .unwrap();
-
-    sleep(Duration::from_secs(1));
-    //cancel example
-    let g = co!(|| {
-        defer!(|| { println!("cancel done!") });
-        for idx in 0..1000 {
-            sleep(Duration::from_secs(1));
-            println!("{}", idx);
-        }
-    });
-    sleep(Duration::from_secs(2));
-    g.coroutine().cancel();
+    // co!(2 * 4096, || {
+    //     println!(
+    //         "coroutine with stack size: {}",
+    //         mco::coroutine::current().stack_size()
+    //     );
+    // });
+    // (2 * 4096).spawn(|| {
+    //     println!(
+    //         "coroutine with stack size: {}",
+    //         mco::coroutine::current().stack_size()
+    //     );
+    // });
+    // co!("coroutine", || {
+    //     println!(
+    //         "coroutine with name: {}",
+    //         mco::coroutine::current().name().unwrap_or_default()
+    //     );
+    // });
+    // "coroutine".spawn(|| {
+    //     println!(
+    //         "coroutine with name: {}",
+    //         mco::coroutine::current().name().unwrap_or_default()
+    //     );
+    // });
+    // co!(Builder::new(), || {
+    //     println!("coroutine with Builder");
+    // });
+    // Builder::new().spawn(|| {
+    //     println!("coroutine with Builder::spawn");
+    // });
+    // spawn(|| {
+    //     println!("coroutine with method spawn");
+    // });
+    // //yield example
+    // co!(move || {
+    //     println!("hi, I'm parent");
+    //     let v = (0..100)
+    //         .map(|i| {
+    //             co!(move || {
+    //                 println!("hi, I'm child{:?}", i);
+    //                 yield_now();
+    //                 println!("bye from child{:?}", i);
+    //             })
+    //         })
+    //         .collect::<Vec<_>>();
+    //     yield_now();
+    //     // wait child finish
+    //     for i in v {
+    //         i.join().unwrap();
+    //     }
+    //     println!("bye from parent");
+    // })
+    // .join()
+    // .unwrap();
+    //
+    // sleep(Duration::from_secs(1));
+    // //cancel example
+    // let g = co!(|| {
+    //     defer!(|| { println!("cancel done!") });
+    //     for idx in 0..1000 {
+    //         sleep(Duration::from_secs(1));
+    //         println!("{}", idx);
+    //     }
+    // });
+    // sleep(Duration::from_secs(2));
+    // g.coroutine().cancel();
 }
